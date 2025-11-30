02e1394697cd921cc120a712833afce1e925c8c4
Alex Wey <alexwey@mac.mynetworksettings.com>
2025-11-30 18:09:37 -0500

    Clean up env variables

--- backend/env.ts
+++ backend/env.ts
@@ -1,4 +1,4 @@
 export const env = {
-  CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY || 'sk_test_vgmVghUesSDCpXwJKMQK5wdMMbVbbEuTe72jJkOx0I',
-  CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY || 'pk_test_aW50ZXJuYWwtaGVycmluZy00MS5jbGVyay5hY2NvdW50cy5kZXYk',
+  CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+  CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
 };
